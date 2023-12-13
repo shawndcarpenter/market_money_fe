5f4ece058f8ab85da3bec10abcf8c21aa2526a80
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-12-13 16:32:21 -0700

    Complete testing for user story 2, market show

--- spec/features/markets/show_spec.rb
+++ spec/features/markets/show_spec.rb
@@ -9,8 +9,18 @@ RSpec.describe "Market" do
       expect(page).to have_content("1400 U Street NW Washington, District of Columbia 20009")
     end
 
-    xit "has the market's vendors" do
+    it "has the market's vendors" do
+      visit "/markets/322482"
+
+      expect(page).to have_link("Saddle Up Smoothies")
+      expect(page).to have_link("Bookish Bazaar")
+      expect(page).to have_link("The Freshly Squeezed Company")
+      expect(page).to have_link("The Sourdough Queen")
+      expect(page).to have_link("Colorful Comforts")
+
+      click_link("Saddle Up Smoothies")
       
+      expect(current_path).to eq("/vendors/55340")
     end
   end
 end
